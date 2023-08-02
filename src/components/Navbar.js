6b556b6b7acc1847dad043a08706e86eff95559a
Diosdado Umali Jr <diosdado_umali_jr@dlsl.edu.ph>
2023-08-02 06:35:08 -0700

    Add my-portfolio to Home in Navbar.js Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         </button>
     </div>
     <div className="links">
-        <Link to="/"> Home </Link>
+        <Link to="/my-portfolio"> Home </Link>
         <Link to="/projects"> Projects </Link>
         <Link to="/experience"> Experience </Link>
     </div>
